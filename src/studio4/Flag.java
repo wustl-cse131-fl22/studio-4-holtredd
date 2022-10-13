fe72c6ecde4bf807b8356239c9e904917748970c
kamilaredd <kamilaredd@localhost>
2022-10-13 13:58:32 -0500

    Kamila Redd and Taylor Holt - Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,39 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color teal = new Color(0, 171, 150);
+		StdDraw.setPenColor(teal);
+		StdDraw.filledCircle(0.5, 0.5, 0.37);
+		
+		Color purple = new Color(136, 123, 237);
+		StdDraw.setPenColor(purple);
+		StdDraw.filledCircle(0.5, 0.5, 0.27);
+		
+		Color bluey = new Color(16, 181, 255);
+		StdDraw.setPenColor(bluey);
+		StdDraw.filledCircle(0.5, 0.5, 0.17);
+		
+		Color reddish = new Color(255, 44, 64);
+		StdDraw.setPenColor(reddish);
+		StdDraw.filledCircle(0.5, 0.5, 0.07);
+		
+		StdDraw.filledCircle(0.5, 0.94, 0.05);
+		StdDraw.filledCircle(0.1, 0.1, 0.05);
+		StdDraw.filledCircle(0.9, 0.1, 0.05);
+		
+		Color black = new Color(0, 0, 0);
+		StdDraw.setPenColor(black);
+		StdDraw.line(0.1, 0.1, 0.5, 0.94);
+		StdDraw.line(0.5, 0.94, 0.9, 0.1);
+		StdDraw.line(0.1, 0.1, 0.9, 0.1);
+		
+		double radius = .02;
+		StdDraw.setPenRadius(radius);
+		StdDraw.rectangle(.5, .5, 0.49, 0.49);
+	}
+
+	private static void setPenRadius(double d) {
+		// TODO Auto-generated method stub
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,29 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		//get info from file
+		String shapeType = in.next();
+		System.out.println(shapeType);
+		int redComponent = in.nextInt();
+		System.out.println(redComponent);
+		int greenComponent = in.nextInt();
+		System.out.println(greenComponent);
+		int blueComponent = in.nextInt();
+		System.out.println(blueComponent);
+		boolean isFilled = in.nextBoolean();
+		System.out.println(isFilled);
+		double parameterOne = in.nextDouble();
+		System.out.println(parameterOne);
+		double parameterTwo = in.nextDouble();
+		System.out.println(parameterTwo);
+		double parameterThree = in.nextDouble();
+		System.out.println(parameterThree);
+		double parameterFour = in.nextDouble();
+		System.out.println(parameterFour);
+		
+		//draw it
+		Color black = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(black);
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
 	}
 }
